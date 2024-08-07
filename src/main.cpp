c4707fcab83efeb85be63ae340626fb385f41ad9
Ishant <ishantpundir202@gmail.com>
2024-08-07 15:17:16 +0530

    Parsing codes done, it should work now :-P

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,66 @@ void setup() {
   Serial.begin(9600);
 
   // Initialize motors...
-  base = Stepper(2, 3, 4);
-  xAxis = Neck(5, 15, 15, 100);
+  base = Stepper(2, 3, 4); // Step-pin, Dir-pin, Enable-pin
+  xAxis = Neck(5, 15, 15, 100); // Servo-pin, max-range, min-range, center-position;
   yAxis = Neck(6, 15, 15, 85);
 
   // Set Neck to center pos!
   xAxis.center();
   yAxis.center();
+}
+
+void handleStepperCommand(char action, String parameters) {
+  int angle, speed;
+  switch (action) {
+    case 'T':
+      sscanf(parameters.c_str(), "%d %d", &angle, &speed);
+      base.turn(angle, speed);
+      break;
+    case 'R':
+      base.release();
+      break;
+    default:
+      Serial.println("Invalid Stepper Action");
+  }
+}
+
+void handleServoCommand(Neck& servo, char action, String parameters) {
+  int value;
+  switch (action) {
+    case 'C':
+      servo.center();
+      break;
+    case 'T':
+      value = parameters.toInt();
+      servo.turnToAngle(value);
+      break;
+    case 'S':
+      value = parameters.toInt();
+      servo.step(value);
+      break;
+    default:
+      Serial.println("Invalid Servo Action");
+  }
+}
+
+void processCommand(String command) {
+  char motorType = command.charAt(0);
+  char action = command.charAt(2);
+  
+  switch (motorType) {
+    case 'S':
+      handleStepperCommand(action, command.substring(4));
+      break;
+    case 'X':
+      handleServoCommand(xAxis, action, command.substring(4));
+      break;
+    case 'Y':
+      handleServoCommand(yAxis, action, command.substring(4));
+      break;
+    default:
+      Serial.println("Invalid Motor Type");
+  }
 }
 
 void loop() {
@@ -29,17 +82,7 @@ void loop() {
     // Read incoming bytes
     command = "";
     command = Serial.readStringUntil('\n');
-
-    int angle = command.toInt();
-    Serial.print("\nTurning to ");
-    Serial.println(angle);
-
-    if (angle == 999) {
-      base.release();
-    }
-    else {
-      base.turn(angle, 100);
-    }
+    processCommand(command);
   }
 
   base.run();
